fff6aaddbb43820bb263fb939c1353fa6fa4c750
IMJEONGSU <3353513a@gmail.com>
2025-11-20 17:43:39 +0900

    16번 sql rankings와 enum 수정- 포인트, 승률, 연승 랭킹 종류 분할

--- backend/src/main/java/org/usyj/makgora/enums/RankingType.java
+++ backend/src/main/java/org/usyj/makgora/enums/RankingType.java
@@ -1,5 +1,7 @@
 package org.usyj.makgora.enums;
 
 public enum RankingType {
-    POINTS, ACCURACY
+    POINTS,        // 포인트 랭킹
+    WINRATE,       // 승률(정답률, accuracy)
+    STREAK         // 연승 랭킹
 }
