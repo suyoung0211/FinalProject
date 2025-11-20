510c445dd81e385af3316204577532c2f42f331e
suyoung0211 <suyoung332211@gmail.com>
2025-11-20 13:58:08 +0900

    backend add

---
+++ makgora/src/main/java/org/usyj/makgora/MakgoraApplication.java
@@ -0,0 +1,13 @@
+package org.usyj.makgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakgoraApplication.class, args);
+	}
+
+}
