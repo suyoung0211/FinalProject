8af270c2672596e11b3c3c0a997d4dbe881623ca
suyoung0211 <suyoung332211@gmail.com>
2025-12-05 18:39:23 +0900

    RssModify

--- backend/src/main/java/org/usyj/makgora/rssfeed/service/RssFeedcollectAndSaveService.java
+++ backend/src/main/java/org/usyj/makgora/rssfeed/service/RssFeedcollectAndSaveService.java
@@ -27,7 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class RssFeedService {
+public class RssFeedcollectAndSaveService {
 
     private final List<RssFeedSource> sources;
     private final RssFeedManagementService feedService;
