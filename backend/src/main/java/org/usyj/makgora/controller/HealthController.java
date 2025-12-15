b25148af6e5386ca14fa07e584953a60faab35b5
kimminjae <khwhj3577@gmail.com>
2025-12-15 21:11:28 +0900

    인포로 변경

--- backend/src/main/java/org/usyj/makgora/controller/HealthController.java
+++ backend/src/main/java/org/usyj/makgora/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    public String health() {
+    public String health() {    
         return "OK";
     }
 }
